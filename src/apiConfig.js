ae1f326d50cd56f2857857f64fe28627ac5a11f0
mp4 <puleri.mp4@gmail.com>
2021-01-14 10:19:26 -0800

    Removed trailing '/' from apiConfig
    Debugging deployment error.

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://polar-hamlet-87107.herokuapp.com/',
+  production: 'https://polar-hamlet-87107.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
